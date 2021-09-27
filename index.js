6196fc6c5dd729daead2c233c357de6aa23904c9
AJPascoe <pascoe312@gmail.com>
2021-09-27 12:28:10 +0100

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const cors = require("cors");
 
 
 dotenv.config();
@@ -24,6 +25,7 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(cors());
 
 app.use("/api/users" , userRoute);
 app.use("/api/auth" , authRoute);
